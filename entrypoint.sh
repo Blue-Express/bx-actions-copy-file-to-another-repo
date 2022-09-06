8556b1ab8e6d1ddecf007b35e0ca0a19ee314558
Jean-Paul <arktyle@Arktyle.localdomain>
2022-09-06 18:09:45 -0300

    Second parameter - source repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,28 +9,36 @@ then
   return -1
 fi
 
+if [ -z "$INPUT_SOURCE_BRANCH" ]
+then
+  INPUT_SOURCE_BRANCH=master
+fi
+OUTPUT_BRANCH_S="$INPUT_SOURCE_BRANCH"
+
 if [ -z "$INPUT_DESTINATION_BRANCH" ]
 then
-  INPUT_DESTINATION_BRANCH=main
+  INPUT_DESTINATION_BRANCH=master
 fi
-OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
+OUTPUT_BRANCH_D="$INPUT_DESTINATION_BRANCH"
 
-CLONE_DIR=$(mktemp -d)
+CLONE_DIR_S=$(mktemp -d)
+CLONE_DIR_D=$(mktemp -d)
 
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch $INPUT_SOURCE_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_SOURCE_REPO.git" "$CLONE_DIR_S"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR_D"
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-cp -R $INPUT_SOURCE_FILE "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-cd "$CLONE_DIR"
+mkdir -p $CLONE_DIR_D/$INPUT_DESTINATION_FOLDER
+cp -R "$CLONE_DIR_S/$INPUT_SOURCE_FILE" "$CLONE_DIR_D/$INPUT_DESTINATION_FOLDER"
+cd "$CLONE_DIR_D"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
   git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
-  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
+  OUTPUT_BRANCH_D="$INPUT_DESTINATION_BRANCH_CREATE"
 fi
 
 if [ -z "$INPUT_COMMIT_MESSAGE" ]
@@ -44,7 +52,7 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
-  git push -u origin HEAD:$OUTPUT_BRANCH
+  git push -u origin HEAD:$OUTPUT_BRANCH_D
 else
   echo "No changes detected"
 fi
